9b86aff4c0cd37ca41b991918e984c540782c032
joeypiccola <joey.piccola@gmail.com>
2018-10-15 20:39:39 -0600

    awful hack

--- tasks/wait_lastbootuptime.ps1
+++ tasks/wait_lastbootuptime.ps1
@@ -33,23 +33,55 @@ Function Get-PuppetNodeFact {
     Write-Output $content.value
 }
 
-$lastBootUpTime_current = Get-PuppetNodeFact -Token $puppetdbapitoken -Master $puppetmaster -Node $node -Fact 'lastbootuptime_wmi'
-
-# create a timespan
-$timespan = New-TimeSpan -Seconds $timeout
-# start a timer
-$stopwatch = [diagnostics.stopwatch]::StartNew()
-
-while ($stopwatch.elapsed -le $timespan) {
-    $lastBootUpTime_now = Get-PuppetNodeFact -Token $puppetdbapitoken -Master $puppetmaster -Node $node -Fact 'lastbootuptime_wmi'
-    if ($lastBootUpTime_current -ne $lastBootUpTime_now) {
-        $stopwatch.stop()
-        exit 0
+function Disable-SslVerification {
+    if (-not ([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+        Add-Type -TypeDefinition  @"
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+public static class TrustEverything
+{
+    private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain,
+        SslPolicyErrors sslPolicyErrors) { return true; }
+    public static void SetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = ValidationCallback; }
+    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; }
+}
+"@
     }
-    Start-Sleep -Seconds 30
+    [TrustEverything]::SetCallback()
 }
 
-if ($stopwatch.elapsed -ge $timespan) {
-    Write-Error "Timeout of $timeout`s has been exceeded. Time elapsed $($stopwatch.Elapsed.Seconds)."
-    exit 1
+function Enable-SslVerification {
+    if (([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+        [TrustEverything]::UnsetCallback()
+    }
+}
+
+try {
+    # allow system to trust untrusted certs
+    Disable-SslVerification
+    $lastBootUpTime_current = Get-PuppetNodeFact -Token $puppetdbapitoken -Master $puppetmaster -Node $node -Fact 'lastbootuptime_wmi'
+
+    # create a timespan
+    $timespan = New-TimeSpan -Seconds $timeout
+    # start a timer
+    $stopwatch = [diagnostics.stopwatch]::StartNew()
+
+    while ($stopwatch.elapsed -le $timespan) {
+        $lastBootUpTime_now = Get-PuppetNodeFact -Token $puppetdbapitoken -Master $puppetmaster -Node $node -Fact 'lastbootuptime_wmi'
+        if ($lastBootUpTime_current -ne $lastBootUpTime_now) {
+            $stopwatch.stop()
+            exit 0
+        }
+        Start-Sleep -Seconds 30
+    }
+
+    if ($stopwatch.elapsed -ge $timespan) {
+        Write-Error "Timeout of $timeout`s has been exceeded. Time elapsed $($stopwatch.Elapsed.Seconds)."
+        exit 1
+    }
+} catch {
+    Write-Error $_.Exception.Message
+} finally {
+    # returns system to normal cert trusting
+    Enable-SslVerification
 }
