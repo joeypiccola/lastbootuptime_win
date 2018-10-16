9358f51cd6da0acf132c965433262a0551540a58
joeypiccola <joey.piccola@gmail.com>
2018-10-16 09:51:44 -0600

    simply facts

--- facts.d/lastbootuptime_wmi.ps1
+++ facts.d/lastbootuptime_wmi.ps1
@@ -1,2 +1,2 @@
 $query = Get-WmiObject -class Win32_OperatingSystem | Select-Object @{ Name = 'LastBootUpTime'; Expression = { ($_.ConvertToDateTime($_.LastBootUpTime)).tostring() }}, @{ Name = 'lastbootuptime_wmi'; Expression = { $_.LastBootUpTime.tostring() }}
-Write-Output Write-Output "lastbootuptime=$($query.LastBootUpTime)"
+Write-Output Write-Output "lastbootuptime_wmi=$($query.lastbootuptime_wmi)"
